2df957769f8a24b51f3921ad1180ad3ba66f1d26
Nicolas Ramel <nramel@r-numerique.fr>
2017-06-20 11:35:12 +0200

    Refactoring code

--- models/Zeapps_project_card_comments.php
+++ models/Zeapps_project_card_comments.php
@@ -2,9 +2,9 @@
 class Zeapps_project_card_comments extends ZeModel {
 
     public function insert($objData = null){
-        $this->load->model('Zeapps_users', 'users');
+        $this->_pLoad->model('Zeapps_users', 'users');
 
-        if($user = $this->load->ctrl->users->getUserByToken($this->load->ctrl->session->get('token'))){
+        if($user = $this->_pLoad->ctrl->users->getUserByToken($this->_pLoad->ctrl->session->get('token'))){
             $objData['id_user'] = $user[0]->id;
             $objData['name_user'] = $user[0]->firstname[0] . '. ' . $user[0]->lastname;
         }
