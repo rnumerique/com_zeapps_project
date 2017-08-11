6ce8abc78e3cf56c3930a8a048ce2e34ac032fcb
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-08-11 16:13:11 +0200

    change projectAuth to hide elements until it can evaluate the rights

--- angularjs/projectAuth.directive.js
+++ angularjs/projectAuth.directive.js
@@ -6,15 +6,29 @@ app.directive("projectAuth", function($rootScope){
             projectAuth: '='
 		},
 		link: function(scope, elm){
-			scope.$watch("projectAuth", function(value){
-				evaluateRight(value.id_project, value.right, elm);
+            elm.hide();
+
+            scope.$watch("projectAuth", function(value){
+            	if(value) {
+                    evaluateRight(value.id_project, value.right, elm);
+                }
+			}, true);
+            $rootScope.$watch("project_rights", function(value){
+            	if(value) {
+                    evaluateRight(scope.projectAuth.id_project, scope.projectAuth.right, elm);
+                }
 			}, true);
 		}
 	};
 
 	function evaluateRight(id_project, right, elm){
-		if($rootScope.project_rights[id_project] && $rootScope.project_rights[id_project][right] !== "1"){
-			elm.remove();
+		if($rootScope.project_rights[id_project]){
+			if($rootScope.project_rights[id_project][right] !== "1") {
+                elm.remove();
+            }
+            else{
+				elm.show();
+			}
 		}
 	}
 
