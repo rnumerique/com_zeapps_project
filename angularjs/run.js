42b403fbfb568c1eef113154887db8ce9aa3a4f7
ThomasSauques <developpeur@Nicolass-Mac-mini.local>
2017-10-06 12:13:27 +0200

    Loading context in a single request through server-side triggers

--- angularjs/run.js
+++ angularjs/run.js
@@ -1,22 +1,11 @@
-app.run(["zeHttp", "$rootScope", function(zhttp, $rootScope){
-	$rootScope.project_rights = [];
-    zhttp.project.right.get_connected().then(function(response){
-		if(response.data && response.data != "false"){
-			$rootScope.project_rights = response.data;
+app.run(["zeHttp", "$rootScope", "$timeout", function(zhttp, $rootScope, $timeout){
+    var watch = $rootScope.$watch("contextLoaded", function(value, oldValue){
+		if(value && value !== oldValue){
+			console.log(value);
+			$timeout(function(){
+                zhttp.project.timer.start();
+			},0);
+			watch();
 		}
 	});
-    zhttp.project.status.get_all().then(function(response){
-		if(response.data && response.data != "false"){
-			$rootScope.statuses = response.data;
-			angular.forEach($rootScope.statuses, function(status){
-				status.sort = parseInt(status.sort);
-			});
-		}
-	});
-    zhttp.project.timer.get_ongoing().then(function(response){
-        if(response.data && response.data != "false"){
-            $rootScope.currentTimer = response.data;
-            zhttp.project.timer.start();
-        }
-    });
 }]);
